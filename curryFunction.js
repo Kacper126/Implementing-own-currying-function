7b2ab0e11e1168213a9e81aadbede2c2a53bc1e8
Kacper <77011712+Kacper126@users.noreply.github.com>
2022-11-19 12:40:51 +0100

    uploading JS file

---
+++ curryFunction.js
@@ -0,0 +1,11 @@
+function curry(func) {
+    return function curriedFunction(...args) {
+        if (args.length >= func.length) {
+            return func.apply(this, args);
+        } else {
+            return function(...args2) {
+                return curriedFunction.apply(this, args.concat(args2));
+            }
+        }
+    }
+}
